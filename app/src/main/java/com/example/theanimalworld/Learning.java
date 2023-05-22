8e4397d3a8167e38d566bb0c0818396cf3941697
Hacini Akram Houssam Eddine <akramhoussamhacini@gmail.com>
2023-05-22 21:58:14 +0100

    Edite learning layout and some issues

--- app/src/main/java/com/example/theanimalworld/Learning.java
+++ app/src/main/java/com/example/theanimalworld/Learning.java
@@ -3,15 +3,19 @@ package com.example.theanimalworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Learning extends AppCompatActivity {
     private Handler handler;
-    TextView text1, text2, text3, text4, text5;
+
+    Button button;
+    TextView text1;
     ImageView img1, img2, img3, img4, img5;
 
     @SuppressLint("MissingInflatedId")
@@ -25,56 +29,84 @@ public class Learning extends AppCompatActivity {
         img4 = findViewById(R.id.kalb);
         img5 = findViewById(R.id.himar);
         text1 = findViewById(R.id.text1);
-        text2 = findViewById(R.id.text2);
-        text3 = findViewById(R.id.text3);
-        text4 = findViewById(R.id.text4);
-        text5 = findViewById(R.id.text5);
+        button = findViewById(R.id.gototest);
+
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                block_img();
                 handler = new Handler();
-                showTextViewForDuration(1000, text1);
+                showTextViewForDuration(2000, "هذه دجاجة");
             }
         });
         img2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 handler = new Handler();
-                showTextViewForDuration(1000, text2);
+                block_img();
+                showTextViewForDuration(2000, "هذا حصان");
             }
         });
         img3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 handler = new Handler();
-                showTextViewForDuration(1000, text3);
+                block_img();
+                showTextViewForDuration(2000, "هذاديك رومي");
             }
         });
         img4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 handler = new Handler();
-                showTextViewForDuration(1000, text4);
+                block_img();
+                showTextViewForDuration(2000, "هذا كلب");
             }
         });
         img5.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                block_img();
                 handler = new Handler();
-                showTextViewForDuration(1000, text5);
+                showTextViewForDuration(2000, "هذا حمار");
             }
         });
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Learning.this,Landlevel1.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void showTextViewForDuration(long duration, TextView textView) {
-        textView.setVisibility(View.VISIBLE);
+
+    public void block_img(){
+        img1.setClickable(false);
+        img2.setClickable(false);
+        img3.setClickable(false);
+        img4.setClickable(false);
+        img5.setClickable(false);
+    }
+    public void unblock_img(){
+        img1.setClickable(true);
+        img2.setClickable(true);
+        img3.setClickable(true);
+        img4.setClickable(true);
+        img5.setClickable(true);
+    }
+    private void showTextViewForDuration(long duration, String textView) {
+        text1.setText(textView);
+        text1.setVisibility(View.VISIBLE);
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                textView.setVisibility(View.INVISIBLE);
+                text1.setVisibility(View.INVISIBLE);
+                unblock_img();
             }
         }, duration);
+
     }
 }
